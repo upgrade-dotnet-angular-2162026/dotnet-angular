bd759fd53e73b9ddfa5ebdfe2a26d57142e2295a
santuparsi <santuparsi@gmail.com>
2026-03-24 15:31:04 +0530

    commit_3_24_3

---
+++ Ado.Net/HandsOn/HandsOnAdo-Demo1/Demo3.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+namespace HandsOnAdo_Demo1
+{
+    internal class Demo3
+    {
+        //invoking stored procedure using ado.net
+        public static void AddEmployee(string name,string dept,int salary)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection("Data Source=DESKTOP-4O1D65I\\SQLEXPRESS;Initial Catalog=bankDb;Integrated Security=True;Trust Server Certificate=True"))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("AddEmployee", connection);
+                    //set Procedur
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    //pass parameter values to store procedure
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Dept", dept);
+                    command.Parameters.AddWithValue("@Salary", salary);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static void GetEmployeesByDept(string dept)
+        {
+            try
+            {
+                using(SqlConnection connection=new SqlConnection("Data Source=DESKTOP-4O1D65I\\SQLEXPRESS;Initial Catalog=bankDb;Integrated Security=True;Trust Server Certificate=True"))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("GetEmployeeByDept", connection);
+                    //set Procedur
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    //pass parameter values to store procedure
+                    command.Parameters.AddWithValue("@deptname", dept);
+                    SqlDataReader reader=command.ExecuteReader();
+                   
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Id:{reader["EmployeeId"]} Name:{reader["Name"]} " +
+                            $"Dept:{reader["Department"]} Salary:{reader[3]}");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        static void Main()
+        {
+            try
+            {
+                AddEmployee("Karan", "Hr", 50000);
+                GetEmployeesByDept("Hr");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Ado.Net/HandsOn/HandsOnAdo-Demo1/Demo4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using System.Data;
+namespace HandsOnAdo_Demo1
+{
+    internal class Demo4
+    {
+        static void Main()
+        {
+            //Dataset
+            //Dataset is a collection of DataTable
+            //Each DataTable can hold 1 result set data
+            using (SqlConnection connection = new SqlConnection("Data Source=DESKTOP-4O1D65I\\SQLEXPRESS;Initial Catalog=bankDb;Integrated Security=True;Trust Server Certificate=True"))
+            {
+                connection.Open();
+                //Initiate Dataset
+                DataSet ds=new DataSet();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("Select * from Employees", connection);
+                //stored result set data in dataset
+                 sqlDataAdapter.Fill(ds,"Employee");
+                connection.Close();
+                foreach(DataRow row in ds.Tables["Employee"].Rows)
+                {
+                    Console.WriteLine($"Id:{row["EmployeeId"]} Name:{row["Name"]}");
+                }
+                Console.WriteLine();
+                //read 2nd row
+                DataRow r = ds.Tables["Employee"].Rows[4];
+                Console.WriteLine($"Id:{r["EmployeeId"]} Name:{r["Name"]}");
+                Console.WriteLine();
+                //holding 2nd table in dataset
+                 sqlDataAdapter = new SqlDataAdapter("Select * from Customers", connection);
+                
+                sqlDataAdapter.Fill(ds, "Customer");
+                DataRow row1 = ds.Tables["Customer"].Rows[2];
+                Console.WriteLine($"Id:{row1["CustomerId"]} Name:{row1["FirstName"]}");
+
+
+            }
+        }
+    }
+}
