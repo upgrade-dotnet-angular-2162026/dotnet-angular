384fea51d2c432da3d7b669620b59365fd5328ec
santuparsi <santuparsi@gmail.com>
2026-03-26 15:09:21 +0530

    commit_3_26_2

---
+++ Asp.net Core/HandsOn/MyHelloMVCApp/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyHelloMVCApp.Controllers
+{
+    public class DemoController : Controller
+    {
+        //Render view page from an anction method
+        public IActionResult Info()
+        {
+            return View(); //View() return view page as a responce
+        }
+        public IActionResult Welcome(string name)
+        {
+            //set value in ViewData
+            ViewData["n"]=name;
+            return View();
+        }
+        public IActionResult LoadData()
+        {
+            ViewData["flowers"] = new List<string> { "Rose", "Lilly", "Jasmie" };
+            return View();
+        }
+    }
+}
---
+++ Asp.net Core/HandsOn/MyHelloMVCApp/Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyHelloMVCApp.Controllers
+{
+    public class SampleController : Controller
+    {
+        public string Welcome()
+        {
+            return "Welcome to MVC World!!";
+        }
+        public string Greet(string name)
+        {
+            return $"Hello,{name}";
+        }
+        public int Add(int a,int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
