d471a5fe4c734fb69f82f1a71a6d1427abff821b
santuparsi <santuparsi@gmail.com>
2026-03-30 17:28:57 +0530

    commit_3_30_5

---
+++ C#.Net/Advance-HandsOn/HandsOnTask/Demo3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnTask
+{
+    internal class Demo3
+    {
+        static void Main()
+        {
+            Task<int> task = Task.Run(() =>
+            {
+                return 1 + 2;
+            });
+            Console.WriteLine(task.Result);
+        }
+    }
+}
---
+++ C#.Net/Advance-HandsOn/HandsOnTask/Demo4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnTask
+{
+    internal class Demo4
+    {
+
+        static void Main()
+        {
+            //create task using constructor
+            Task t1 = new Task(() =>
+            {
+                Console.WriteLine("Welcome to Task Programming!!");
+            });
+            t1.Start(); //start the task
+            t1.Wait(); //It waits main thread to complete the task t1
+            Console.WriteLine("Task Completed");
+        }
+    }
+}
