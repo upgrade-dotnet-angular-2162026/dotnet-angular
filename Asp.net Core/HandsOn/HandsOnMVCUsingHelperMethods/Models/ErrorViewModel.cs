b4fff22af4d0a4bf54a749df4f320d9310ac8921
santuparsi <santuparsi@gmail.com>
2026-03-30 11:18:16 +0530

    commit_3_30_2

---
+++ Asp.net Core/HandsOn/HandsOnMVCUsingHelperMethods/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HandsOnMVCUsingHelperMethods.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Asp.net Core/HandsOn/HandsOnMVCUsingHelperMethods/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace HandsOnMVCUsingHelperMethods.Models
+{
+    public class Login
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
