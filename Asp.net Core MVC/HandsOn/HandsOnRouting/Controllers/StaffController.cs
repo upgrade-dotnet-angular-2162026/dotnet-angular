7ee754e4b56d36062192acc660f2166893c2f41c
santuparsi <santuparsi@gmail.com>
2026-03-31 10:03:49 +0530

    commit_3_31_1

---
+++ Asp.net Core MVC/HandsOn/HandsOnRouting/Controllers/StaffController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HandsOnRouting.Controllers
+{
+    public class StaffController : Controller
+    {
+        [Route("Staff/GetAllStaff")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("AddStaff")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [Route("GetStaff/{id}")]
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.net Core MVC/HandsOn/HandsOnRouting/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HandsOnRouting.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Student/GetStudent/{id:int}")]
+        public IActionResult Details(int Id)
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        public IActionResult Edit()
+        {
+            return View();  
+        }
+        public IActionResult Delete(int Id)
+        {
+            return View();
+        }
+    }
+}
