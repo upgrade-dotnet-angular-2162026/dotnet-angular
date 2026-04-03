869c12f05c8ae683c46f1ee6f613569597972c13
santuparsi <santuparsi@gmail.com>
2026-04-03 13:30:29 +0530

    commit_4_3_4

---
+++ C#.Net/HandsOn/MyApp.Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using Myapp.CalculateLibrary;
+using System.Net.Http.Headers;
+namespace MyApp.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate obj = new Calculate();
+            Console.WriteLine(obj.Add(2, 3));
+            Console.WriteLine(obj.Mul(4, 5));
+            Console.WriteLine(obj.Sub(10, 2));
+            Console.WriteLine(obj.Square(3));
+        }
+    }
+}
---
+++ C#.Net/HandsOn/Myapp.CalculateLibrary/Calculate.cs
@@ -0,0 +1,22 @@
+﻿namespace Myapp.CalculateLibrary
+{
+    public class Calculate
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public int Mul(int a,int b)
+        {
+            return a * b;
+        }
+        public int Sub(int a,int b)
+        {
+            return a - b;
+        }
+        public int Square(int k)
+        {
+            return k * k;
+        }
+    }
+}
