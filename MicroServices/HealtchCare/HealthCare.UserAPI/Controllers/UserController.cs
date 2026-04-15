9002469db54616d39b5e0607c336187527f36f66
santuparsi <santuparsi@gmail.com>
2026-04-15 12:31:17 +0530

    commit_4_15_4

---
+++ MicroServices/HealtchCare/HealthCare.UserAPI/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCare.UserAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GetUser")]
+        public IActionResult GetAll()
+        {
+            return Ok(new string[] { "Rohan", "Karan", "Kavay" });
+        }
+    }
+}
