de10a5ca70bdd3b24065d2d0e9ab9d1a9c10927a
santuparsi <santuparsi@gmail.com>
2026-04-17 10:47:46 +0530

    commit_4_17_1

---
+++ UnitTesting/UnitTestingDemoUsingNunit/MathLibrary/Calculate.cs
@@ -0,0 +1,22 @@
+﻿namespace MathLibrary
+{
+    public class Calculate
+    {
+        public int Add(int a, int b)
+        {
+            int result = a+b;
+            return result;
+        }
+        public bool IsEven(int a)
+        {
+            if(a%2==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ UnitTesting/UnitTestingDemoUsingNunit/TestMathLibrary/CalculateTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit;
+using MathLibrary;
+namespace TestMathLibrary
+{
+    [TestFixture]
+    public class CalculateTest
+    {
+        private Calculate ?calculate=null;
+        [SetUp] //exexute before every test method execution
+        public void SetUp()
+        {
+            //Arrange
+             calculate = new Calculate();
+        }
+        [TearDown] //execute after every test method execution
+        public void TearDown()
+        {
+            calculate = null;
+        }
+
+        //write test cases here
+        [Test]
+        public void Test_Add()
+        {
+            //Arrange
+            Calculate obj=new Calculate();
+            int expected = 5;
+            //Act
+            int actual = obj.Add(2, 3);
+            //Assert
+           Assert.That(expected,Is.EqualTo(actual));
+        }
+        [Test]
+        public void Test_IsEven_Fail()
+        {
+            //Arrange
+           // Calculate calculate=new Calculate();
+          
+            //Act
+            bool actual = calculate.IsEven(3);
+            //Assert
+            Assert.That(actual, Is.False);
+        }
+        [Test]
+        public void Test_IsEven_Pass()
+        {
+            //Arrange
+            Calculate calculate = new Calculate();
+
+            //Act
+            bool actual = calculate.IsEven(4);
+            //Assert
+            Assert.That(actual, Is.True);
+        }
+
+    }
+}
